4d2074ec3a66373b565c5a2f0ba47946b7a0dbd9
luka <edulukalortk@gmail.com>
2024-10-19 09:21:05 +0000

    posts endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import auth, user, big_transactions, transactions_stats, cryptos 
+from app.routes import auth, user, big_transactions, transactions_stats, posts 
 from app.database import init_db
 from dotenv import load_dotenv
 import os
@@ -26,6 +26,7 @@ app.include_router(auth.router, prefix="/jwt", tags=["authentication"])
 app.include_router(user.router, prefix="/auth", tags=["user"])
 app.include_router(big_transactions.router, prefix="/whales",tags=["websocket"])
 app.include_router(transactions_stats.router, prefix="/stats",tags=["websocket"])
+app.include_router(posts.router, prefix="/posts",tags=["websocket"])
 
 if __name__ == "__main__":
     import uvicorn
